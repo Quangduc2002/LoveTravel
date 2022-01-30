b6ad6f0a911cc75338aba809dc90cb69dbc1cbb0
quangduc2002 <phamquangduc110@gmail.com>
2022-01-30 16:20:35 +0700

    upload

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+
+function menu1(){
+    document.getElementById("menu2").style.right = "0px";
+}
+
+function closes1(){
+    document.getElementById("menu2").style.right = "-400px";
+}
+
+
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        infinite: true,  /*hết ảnh thì quay lại */   
+        arrows: false,   /*mất nút chuyển ảnh */
+        fade: true,
+        cssEase: 'linear',
+        draggable:false,
+        slidesToShow: 1,   /*hiện ảnh */
+        autoplay: true,     /*điều kiện để tự động chạy */
+        autoplaySpeed: 3000,    /* time chạy 2s*/
+        
+    });
+});
